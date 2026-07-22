0cfe0799d255e288067172ca0550fdd34c82d8f6
LAKSHWORKPC\laksh <lakshyatyagi8@gmail.com>
2026-07-22 15:28:38 +0100

    UI TEST FIX

--- accessories-web/src/app/app.spec.ts
+++ accessories-web/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, accessories-web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Computer Accessories Inventory');
   });
 });
