28ba3b9bb9669a00b0415b80756131b23cc0386a
LAKSHWORKPC\laksh <lakshyatyagi8@gmail.com>
2026-07-22 16:47:21 +0100

    policy.WithOrigins("http://localhost:4200", "http://192.168.1.18:4200")

--- Accessories.Api/Program.cs
+++ Accessories.Api/Program.cs
@@ -28,7 +28,7 @@ public class Program
             options.AddPolicy("AllowAngularFrontend",
                 policy =>
                 {
-                    policy.WithOrigins("http://localhost:4200")
+                    policy.WithOrigins("http://localhost:4200", "http://192.168.1.18:4200")
                         .AllowAnyHeader()
                         .AllowAnyMethod();
                 });
