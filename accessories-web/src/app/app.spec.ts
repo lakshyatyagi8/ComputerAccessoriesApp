2e2434a4e8312fa8a715f956ba561fe80d618cab
LAKSHWORKPC\laksh <lakshyatyagi8@gmail.com>
2026-07-22 14:36:32 +0100

    The Product Type Error and The ./app Import Error

--- accessories-web/src/app/app.spec.ts
+++ accessories-web/src/app/app.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app.component';
 
 describe('App', () => {
   beforeEach(async () => {
@@ -9,13 +9,13 @@ describe('App', () => {
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, accessories-web');
